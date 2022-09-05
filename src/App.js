6438decb57c696abcdeb3f9d9da5d268d24fb6bc
dantelazz <69008060+dantelazz@users.noreply.github.com>
2022-09-05 20:59:54 -0300

    Creando Item Count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import NavBar from './NavBar/NavBar'
+import ItemCount from './ItemCount/ItemCount';
 
 function App() {
   return (
     <div>
       <NavBar/>
+
+      <ItemCount stock={15} initial={1} onAdd={0}/>
     </div>
       
 
---
+++ src/ItemCount/ItemCount.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [itemStock, setItemStock] = useState (stock);
+    const [itemInitial, setItemInitial] = useState (initial);
+    const [itemAdd, setItemAdd] = useState (onAdd);
+
+    const quitarItems = (valor) => {
+        if (valor > 0) {
+            setItemInitial(valor);
+        }
+    }
+    
+    const agregarItems = (valor) => {
+        if (valor <= itemStock) {
+            setItemInitial(valor);
+        }
+    }
+
+    const agregarProducto = () =>{
+        if (itemInitial <= itemStock) {
+            setItemStock (itemStock - itemInitial);
+            setItemAdd (itemAdd + itemInitial)
+        }
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-md-2">
+                    <p className="text-start"></p>
+                    <div className="input-group">
+                        <input className="btn btn-secondary" type="button" value="-" onClick={() =>
+                        {quitarItems(itemInitial - 1)}} />
+                        <input className="form-control" type="text" value={itemInitial} onChange={()=>{}} />
+                        <input className="btn btn-secondary" type="button" value="+" onClick={() =>
+                        {agregarItems(itemInitial + 1)}} />
+                        
+                    </div>
+                    <div className="d-grid pt-3 gap-2">
+                        <input className="btn btn-light" type="button" value="agregar" onClick={() =>
+                        {agregarProducto()}}/>
+                    </div>
+                    <p>Productos seleccionados: {itemAdd}</p>
+                </div>
+            </div>
+        </div>
+    )
+} 
+
+export default ItemCount;
